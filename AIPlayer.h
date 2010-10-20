14f8c154ac3e0c057678c23806afe5c4b1ce1d93
mrKraft <mrkr.reg@gmail.com>
2010-10-20 18:54:37 +0400

    Начал писать искуственный интелект.

---
+++ AIPlayer.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+
+class AIPlayer {
+private:
+
+	double bluff;	// ���������� �����
+	double risk;	// ���������� ����, ��� ������� 
+
+	bool is_fold;	// �������� ��� ���
+	bool is_shared;	// ��� ������� ����� ��� ���
+	int money;
+
+	vector <int> cards_card;
+	vector <int> cards_suit;
+
+public:
+
+	
+};
